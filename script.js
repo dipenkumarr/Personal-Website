30e4cee5644f20ea32211fbccc14485f6a24ff2e
dipen kumar <dipenbachani@gmail.com>
2023-08-23 02:51:36 +0400

    added projects

---
+++ script.js
@@ -0,0 +1,12 @@
+var typed = new Typed("#element", {
+  strings: ["Student.", "Web Developer.", "Photo Editor."],
+  typeSpeed: 50,
+  //   loop: Infinity,
+});
+
+function githubFunction() {
+  window.open("https://github.com/dipenkumarr");
+}
+function resumeFunction() {
+  window.open("Resume-Dipen-Kumar-uofa.pdf");
+}
