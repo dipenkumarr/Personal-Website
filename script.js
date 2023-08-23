e93176cf7bf5cbaea9614960ce11f01ef1a1d02c
Dipen Kumar <73647830+dipenkumarr@users.noreply.github.com>
2023-08-23 04:05:18 +0400

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function githubFunction() {
   window.open("https://github.com/dipenkumarr");
 }
 function resumeFunction() {
-  window.open("Resume-PDF 1.pdf");
+  window.open("Resume PDF 1.pdf");
 }
 function linkedinFunction() {
   window.open("https://www.linkedin.com/in/dipen-kumar");
